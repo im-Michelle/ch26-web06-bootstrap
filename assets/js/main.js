96ea7f801fa48c19f5b1bdbfd8a2c1130f111fea
Michelle Garcia <michelletgar@gmail.com>
2023-04-21 13:51:39 -0600

    ci: botones y parrafos cambian color

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,21 @@
 console.log("Hola desde un script externo");
 
 
-function changeColorToRed(){
+function changeColorTo(color){
     const refName = document.getElementById("name");
-    refName.style.color = "red";
+    refName.style.color = color;
+}
+
+function changeColorParagraph(refObj, color){
+    console.log(refObj);
+    refObj.style.color = color;
+}
+
+function resetColors(){
+    const refObjs = document.getElementsByClassName("text-color");
+    console.log(refObjs);
+    for (let index = 0; index < refObjs.length; index++) {
+        const element = refObjs[index];
+        element.style.color = "purple";
+    }
 }
